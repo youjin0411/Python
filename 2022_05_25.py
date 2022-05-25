c3ec00a90674a55157e6865c524900d0ee8b92b7
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-05-25 22:53:28 +0900

    조건문 문제 풀기

--- 2022_05_25.py
+++ 2022_05_25.py
@@ -68,3 +68,32 @@ elif number > 100 or number == 10:
     print("number가 100보다 크거나 또는 number의 값이 10입니다. ")
 elif not number > 100:
     print("number가 100ㅂ다 큰 경우가 아닌 경우 출력합니다. ")
+
+print()
+
+# 예제1. 점수가 60점 이상이면 "합격"을 출력하고, 미만이면 "불합격"을 출력
+score = 60
+if score >= 60:
+    print("합격")
+else:
+    print("불합격")
+
+result = int(score // 60)
+if result:
+    print("합격")
+else:
+    print("불합격")
+
+# 문제1. 정수를 1개 입력받아서, 60점 이상이면 합격, 미만이면 불합격
+a = int(input("정수 입려 : "))
+if a >= 60:
+    print("합격")
+else:
+    print("불합격")
+
+# 문제2. 정수를 1개 입력받아서, 그 수가 짝수인지 홀수인지
+b = int(input("정수 입력 : "))
+if b & 2 == 0:
+    print("짝수")
+else:
+    print("홀수")
