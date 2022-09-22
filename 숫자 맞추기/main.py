bbdbd722d0d7136c7accde942cfdb8d38f004ba7
youjin <w2117@e-mirim.hs.kr>
2022-09-22 09:05:51 +0900

    test

--- 숫자 맞추기/main.py
+++ 숫자 맞추기/main.py
@@ -1,13 +1,16 @@
+from faulthandler import cancel_dump_traceback_later
 import random
 random_number = random.randint(1, 100)
 print(random_number)
 
+cnt = 1
 while True:
     my_number = int(input("1~100 사이의 수 입력 :"))
-    if my_number >= random_number:
+    if my_number > random_number:
         print("DOWN")
     elif my_number < random_number:
         print("UP")
     else:
-        print("축하합니다. 맞추었습니다")
+        print(f"축하합니다. {cnt}회 만에 맞추었습니다")
         break
+    cnt = cnt + 1
