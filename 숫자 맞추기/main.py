00e68502c0cdb0e8674c285a2e072233274fd458
youjin <w2117@e-mirim.hs.kr>
2022-09-22 09:09:10 +0900

    test

--- 숫자 맞추기/main.py
+++ 숫자 맞추기/main.py
@@ -5,12 +5,15 @@ print(random_number)
 
 cnt = 1
 while True:
-    my_number = int(input("1~100 사이의 수 입력 :"))
-    if my_number > random_number:
-        print("DOWN")
-    elif my_number < random_number:
-        print("UP")
-    else:
-        print(f"축하합니다. {cnt}회 만에 맞추었습니다")
-        break
-    cnt = cnt + 1
+    try:
+        my_number = int(input("1~100 사이의 수 입력 :"))
+        if my_number > random_number:
+            print("DOWN")
+        elif my_number < random_number:
+            print("UP")
+        else:
+            print(f"축하합니다. {cnt}회 만에 맞추었습니다")
+            break
+        cnt = cnt + 1
+    except:
+        print("에러가 발생했습니다. 숫자 1~100를 입력하세요.")
