95e9ca87d0ca78da08d5d5629c754489e4ca3a74
xxx-1203 <s2119@e-mirim.hs.kr>
2022-09-22 09:00:39 +0900

    랜덤 숫자 만들기

--- 숫자 맞추기/main.py
+++ 숫자 맞추기/main.py
@@ -1,3 +1,13 @@
 import random
-random_number = random.randint(1,100)
+random_number = random.randint(1, 100)
 print(random_number)
+
+while True:
+    my_number = int(input("1~100 사이의 수 입력 :"))
+    if my_number >= random_number:
+        print("DOWN")
+    elif my_number < random_number:
+        print("UP")
+    else:
+        print("축하합니다. 맞추었습니다")
+        break
