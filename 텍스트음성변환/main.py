6722b73fca60b73c2652e849ab84314d90e3c47f
youjin <w2117@e-mirim.hs.kr>
2022-09-29 10:07:16 +0900

    python 음성

--- 텍스트음성변환/main.py
+++ 텍스트음성변환/main.py
@@ -1,12 +1,13 @@
+# pip install playsound==1.2.2 재생이 안 될 때 
 from gtts import gTTS  # 텍스트를 음성으로 변환
 from playsound import playsound
 # 텍스트를 음성으로 변환
-file_path = "Mydata.txt"
-with open(file_path, "rt", encoding="UTF8") as f:
+file_path = 'Mydata.txt'
+with open(file_path, 'rt', encoding='UTF8') as f:
     read_file = f.read()
 
-text = "나의 살던 고향은 꽃피는 산골."
-tts = gTTS(text=text, lang="ko")
+# text = "나의 살던 고향은 꽃피는 산골."
+tts = gTTS(text=read_file, lang="ko")
 mp = ("schoolsong.mp3")
 tts.save(mp)
 
